f2ef35ee9a6ec1c8d8a53f5a87693382181acb2d
renatogomesf <renato_gfe@hotmail.com>
2025-11-02 12:12:04 -0300

    test: adding user update tests and correcting others arquives

--- test/login.test.ts
+++ test/login.test.ts
@@ -2,67 +2,68 @@ import { describe, it, expect, beforeAll } from 'vitest';
 import supertest from 'supertest';
 import app from '../src/server';
 import { AppDataSource } from '../src/data-source';
+import { LoginUserDTO } from './../src/dtos/login.dto';
 
-beforeAll(() => {
-    AppDataSource.initialize();
+beforeAll(async () => {
+    await AppDataSource.initialize();
 });
 
 describe('Login test', () => {
-    it('shold login successfully', async () => {
-        const user = {
+    it('should login successfully', async () => {
+        const userLogin: LoginUserDTO = {
             email: 'faisca@gmail.com',
             password: '222',
         };
 
-        const response = await supertest(app).post('/login').send(user);
+        const response = await supertest(app).post('/login').send(userLogin);
 
         expect(response.status).toEqual(200);
         expect(response.body).toHaveProperty('token');
     });
 
-    it('dont shold login successfully | incorrect email or password', async () => {
-        const user = {
+    it('dont should login successfully | incorrect email or password', async () => {
+        const userLogin: LoginUserDTO = {
             email: 'renato@gmail.com',
             password: '222',
         };
 
-        const response = await supertest(app).post('/login').send(user);
+        const response = await supertest(app).post('/login').send(userLogin);
 
         expect(response.status).toEqual(401);
         expect(response.body).toHaveProperty('message', 'incorrect email or password');
     });
 
-    it('dont shold login successfully | password not provided', async () => {
-        const user = {
+    it('dont should login successfully | password not provided', async () => {
+        const userLogin: LoginUserDTO = {
             email: 'faisca@gmail.com',
             password: '',
         };
 
-        const response = await supertest(app).post('/login').send(user);
+        const response = await supertest(app).post('/login').send(userLogin);
 
         expect(response.status).toEqual(400);
         expect(response.body).toHaveProperty('message', 'all fields are required');
     });
 
-    it('dont shold login successfully | email not provided', async () => {
-        const user = {
+    it('dont should login successfully | email not provided', async () => {
+        const userLogin: LoginUserDTO = {
             email: '',
             password: '123',
         };
 
-        const response = await supertest(app).post('/login').send(user);
+        const response = await supertest(app).post('/login').send(userLogin);
 
         expect(response.status).toEqual(400);
         expect(response.body).toHaveProperty('message', 'all fields are required');
     });
 
-    it('dont shold login successfully | Email and password not provided', async () => {
-        const user = {
+    it('dont should login successfully | Email and password not provided', async () => {
+        const userLogin: LoginUserDTO = {
             email: '',
             password: '',
         };
 
-        const response = await supertest(app).post('/login').send(user);
+        const response = await supertest(app).post('/login').send(userLogin);
 
         expect(response.status).toEqual(400);
         expect(response.body).toHaveProperty('message', 'all fields are required');
--- test/userTest/getOneUser.test.ts
+++ test/userTest/getOneUser.test.ts
@@ -3,26 +3,30 @@ import supertest from 'supertest';
 import app from '../../src/server';
 import { AppDataSource } from '../../src/data-source';
 
-beforeAll(() => {
-    AppDataSource.initialize();
+beforeAll(async () => {
+    await AppDataSource.initialize();
 });
 
 describe('Get one user test', () => {
-    it('shold get one user successfully', async () => {
-        const response = await supertest(app).get('/v1/getoneuser/1');
+    it('should get one user successfully', async () => {
+        const allUser = await supertest(app).get('/v1/getalluser');
+
+        const IdOneUser: number = await allUser.body[0].idUser;
+
+        const response = await supertest(app).get(`/v1/getoneuser/${IdOneUser}`);
 
         expect(response.status).toEqual(200);
         expect(response.body).toHaveProperty('idUser');
     });
 
-    it('dont shold get one user successfully | user not found', async () => {
-        const response = await supertest(app).get('/v1/getoneuser/10000');
+    it('dont should get one user successfully | user not found', async () => {
+        const response = await supertest(app).get('/v1/getoneuser/100000');
 
         expect(response.status).toEqual(404);
         expect(response.body).toHaveProperty('message', 'user not found');
     });
 
-    it('dont shold get one user successfully | id not provided', async () => {
+    it('dont should get one user successfully | id not provided', async () => {
         const response = await supertest(app).get('/v1/getoneuser');
 
         expect(response.status).toEqual(404);
